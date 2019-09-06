dd4fa596e2e4179cb24d509a04923d90bb343b53
Igor Pogorelko <ahtyrka@gmail.com>
2019-09-06 13:09:43 +0300

    Update HelloWorld.java

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -12,7 +12,7 @@ public class HelloWorld extends HttpServlet {
 
    public void init() throws ServletException {
       // Do required initialization
-      message = "Hello World";
+      message = "Hello Beatiful World !!!";
    }
 
    public void doGet(HttpServletRequest request, HttpServletResponse response)
